f33fd16a2e20bd4001a0af553d257b8f1498c006
Kyroth88 <kaylen.bartoshewski@gmail.com>
2026-07-17 09:07:53 -0400

    Create long_flights.lua

---
+++ long_flights.lua
@@ -0,0 +1,14 @@
+# This script keeps instant flights defaulted to OFF when server config allows the gossip menu to select instant flights.
+local InstantFlightReset = {}
+
+function InstantFlightReset.OnLogin(event, player)
+    -- Reset any session-only instant flight flag
+    player:SetData("instant_flight", false)
+
+    -- Reset PlayerTaxi instant flight if your gossip uses this
+    if player:GetTaxi() then
+        player:GetTaxi():SetInstantTaxi(false)
+    end
+end
+
+RegisterPlayerEvent(3, InstantFlightReset.OnLogin)
